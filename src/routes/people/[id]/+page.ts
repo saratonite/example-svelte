137badb816253b55b06c81e84a514092f8908e02
Sarath <sarath.tk@icloud.com>
2024-07-24 16:22:00 +0530

    package.json update

--- src/routes/people/[id]/+page.ts
+++ src/routes/people/[id]/+page.ts
@@ -1,8 +1,7 @@
 import { getResourceItem } from '$lib/sw';
 import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ params }) => {
-	let data = getResourceItem('people', params?.id);
-
+	let data = await  getResourceItem('people', params?.id);
 	return {
 		data
 	};
